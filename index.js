9e69827a5d3927a886b37f76ea85ff6a4f397b7f
Gurbanov Javid <melloalter@gmail.com>
2024-07-14 22:19:21 +0400

    JS 1 WORK

---
+++ index.js
@@ -0,0 +1,63 @@
+// const nname = prompt("Введите Имя")
+// alert('Привет '+ nname)
+
+
+
+// const years = prompt('Какого Вы года рождения')
+// const inyears = 2024
+// alert(inyears - years)
+
+
+// const quadrat = prompt('Длина квадрата')
+// alert('Периметр квадрата =' + quadrat*4)
+
+
+// const radius = prompt('Введите радиус круга')
+// alert('Площадь = '+ (3.14*(radius*radius)))
+
+// const km = prompt('Сколько КМ надо проехать')
+// const hours = prompt('За сколько часов')
+// alert('Нужно ехать'+km/hours)
+
+// const dollar = prompt('Введите сумму')
+// const euro = 0.92
+// alert('Евро '+(dollar*euro))
+
+// const gb = prompt('Сколько ГБ')
+// const mb = 820
+// alert(Math.floor(gb*1000/mb))
+
+// const nalik = prompt('Сколько средств')
+// const price = prompt('Цена 1 шоколадки')
+// alert(('Можете купить '+ Math.floor(nalik/price)) +(' Сдача ' + nalik%price))
+
+// const nalik = prompt('Сколько средств')
+// const price = prompt('Цена 1 шоколадки')
+// alert(('Можете купить '+ nalik/price) +(' Сдача ' + nalik%price))
+
+// const num = prompt('Введите число')
+// if (num%2 === 0){
+//     alert('Чётное')
+// }
+// else{
+//     alert('Нечётное')
+// }
+
+// const num = prompt('Введите число')
+// let i = 0
+// while( i < num){
+//     alert('#');
+//     i++;
+// }
+
+// const num = prompt('Введите число')
+// let i = num
+// while(0 <= i){
+//     alert(i);
+//     i--;
+// }
+
+// const num = prompt('Ввидетие число')
+// const step = prompt('Введите степень')
+// alert(num**prompt);
+
